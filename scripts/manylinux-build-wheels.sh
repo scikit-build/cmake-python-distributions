49d4b42481679701015eabf6b39dd3572e3e96b6
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-09-22 01:02:26 -0400

    Add manylinux-build-wheels.sh
    This script has been created to be used from within the quay.io/pypa/manylinux1_x86_64
    or dockcross/manylinux-x64
    
    Here is an example of use with dockcross/manylinux-x64:
    
    docker pull dockcross/manylinux-x64
    docker run dockcross/manylinux-x64 > ~/bin/dockcross-manylinux-x64
    chmod u+x ~/bin/dockcross-manylinux-x64
    git clone git://github.com/jcfr/cmake-python-distributions.git
    cd cmake-python-distributions
    dockcross-manylinux-x64 ./scripts/manylinux-build-wheels.sh

---
+++ scripts/manylinux-build-wheels.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e -x
+
+# Build standalone project and populate archive cache
+mkdir -p /work/archive-cache
+mkdir -p /work/standalone-build
+pushd /work/standalone-build > /dev/null 2>&1
+  cmake -DCMakePythonDistributions_ARCHIVE_DOWNLOAD_DIR:PATH=/work/archive-cache -G Ninja ../
+  ninja
+popd > /dev/null 2>&1
+
+# Compile wheels re-using standalone project and archive cache
+for PYBIN in /opt/python/*/bin; do
+    if [[ ${PYBIN} == *"cp26"* ]]; then
+        echo "Skipping ${PYBIN}"
+        continue
+    fi
+    ${PYBIN}/pip install -r /work/requirements-dev.txt
+    ${PYBIN}/python setup.py bdist_wheel -- \
+      -DCMakePythonDistributions_ARCHIVE_DOWNLOAD_DIR:PATH=/work/archive-cache \
+      -DCMakeProject_BINARY_DIR:PATH=/work/standalone-build
+    ${PYBIN}/python setup.py clean
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    if [[ ${PYBIN} == *"cp26"* ]]; then
+        echo "Skipping ${PYBIN}"
+        continue
+    fi
+    ${PYBIN}/pip install cmake --no-index -f /work/dist
+    (cd $HOME; ${PYBIN}/cmake --version)
+done
