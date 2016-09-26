4faee763482fe262789f01881c65019666927788
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-09-25 22:00:04 -0400

    manylinux-build-wheels: Add support for building x86 and x64 wheels

--- scripts/manylinux-build-wheels.sh
+++ scripts/manylinux-build-wheels.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 set -e -x
 
+# i686 or x86_64 ?
+case $(uname -p) in
+    i686)
+        arch=x86
+        ;;
+    x86_64)
+        arch=x64
+        ;;
+    *)
+        die "Unknown architecture $(uname -p)"
+        ;;
+esac
+
+echo "Building wheels for $arch"
+
 # Build standalone project and populate archive cache
 mkdir -p /work/archive-cache
-mkdir -p /work/standalone-build
-pushd /work/standalone-build > /dev/null 2>&1
+mkdir -p /work/standalone-${arch}-build
+pushd /work/standalone-${arch}-build > /dev/null 2>&1
   cmake -DCMakePythonDistributions_ARCHIVE_DOWNLOAD_DIR:PATH=/work/archive-cache -G Ninja ../
   ninja
 popd > /dev/null 2>&1
@@ -18,13 +33,13 @@ for PYBIN in /opt/python/*/bin; do
     ${PYBIN}/pip install --user -r /work/requirements-dev.txt
     ${PYBIN}/python setup.py bdist_wheel -- \
       -DCMakePythonDistributions_ARCHIVE_DOWNLOAD_DIR:PATH=/work/archive-cache \
-      -DCMakeProject_BINARY_DIR:PATH=/work/standalone-build
+      -DCMakeProject_BINARY_DIR:PATH=/work/standalone-${arch}-build
     ${PYBIN}/python setup.py clean
 done
 
 # Since there are no external shared libraries to bundle into the wheels
 # this step will fixup the wheel switching from 'linux' to 'manylinux1' tag
-for whl in dist/*.whl; do
+for whl in dist/*$(uname -p).whl; do
     auditwheel repair $whl -w /work/dist/
     rm $whl
 done
