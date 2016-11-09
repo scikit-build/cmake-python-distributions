80ff85244240848e4a8ae6a1728e9784d187c22f
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-11-09 11:02:46 -0500

    docs/conf.py: Ensure title include current version

--- docs/conf.py
+++ docs/conf.py
@@ -136,7 +136,7 @@ html_theme = 'alabaster'
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
 #
-# html_title = 'CMake Python Distributions v0.1.0'
+# html_title = 'CMake Python Distributions v%s' % version
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #
