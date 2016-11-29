c9504da9d46ff4ca10b6736e6d1e06770cef1b75
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-11-29 13:17:23 -0500

    tests: Add test_cmake_https. See #15

--- tests/test_cmake.py
+++ tests/test_cmake.py
@@ -1,17 +1,44 @@
 
 import pytest
+import textwrap
 
 import cmake
 
 from . import push_argv
 
 
+def _run(program, args):
+    func = getattr(cmake, program)
+    args = ["%s.py" % program] + args
+    with push_argv(args), pytest.raises(SystemExit) as excinfo:
+        func()
+    assert 0 == excinfo.value.code
+
+
 def test_cmake_module():
-    with push_argv(["cmake.py", "--version"]), pytest.raises(SystemExit):
-        cmake.cmake()
+    _run("cmake", ["--version"])
+    _run("cpack", ["--version"])
+    _run("ctest", ["--version"])
 
-    with push_argv(["cpack.py", "--version"]), pytest.raises(SystemExit):
-        cmake.cpack()
 
-    with push_argv(["ctest.py", "--version"]), pytest.raises(SystemExit):
-        cmake.ctest()
+def test_cmake_https(tmpdir):
+    test_script = tmpdir.join("cmake-test-https-download.cmake")
+    test_script.write(textwrap.dedent(
+        """
+        file(
+          DOWNLOAD
+            https://github.com/scikit-build/cmake-python-distributions
+            ${TMP_DIR}/page.html
+          SHOW_PROGRESS
+          STATUS status
+          )
+        list(GET status 0 error_code)
+        list(GET status 1 error_msg)
+        if(error_code)
+            message(
+                FATAL_ERROR "error: Failed to download ${url}: ${error_msg}")
+        endif()
+        """
+    ))
+
+    _run("cmake", ["-DTMP_DIR:PATH=%s" % str(tmpdir), "-P", str(test_script)])
