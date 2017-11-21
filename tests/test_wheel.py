817e5ba918f04d015487c9c9363481933cab080c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-11-21 00:58:12 -0500

    Update to CMake v3.10.0

--- tests/test_wheel.py
+++ tests/test_wheel.py
@@ -13,7 +13,7 @@ def test_command_line(virtualenv, tmpdir):
 
     virtualenv.run("pip install %s" % wheels[0])
 
-    expected_version = "3.9.4"
+    expected_version = "3.10.0"
 
     for executable_name in ["cmake", "cpack", "ctest"]:
         output = virtualenv.run(
