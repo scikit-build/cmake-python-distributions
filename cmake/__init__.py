afbc83eab4629b3e3e1350ab8c6805bc13af0c24
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-07-09 17:46:14 -0400

    test: Fix execution of tests from source tree
    * Require scikit-build >= 0.7.1 and use skbuild.constants to reference
      CMake binaries.

--- cmake/__init__.py
+++ cmake/__init__.py
@@ -12,8 +12,9 @@ CMAKE_DATA = os.path.join(os.path.dirname(__file__), 'data')
 
 # Support running tests from the source tree
 if not os.path.exists(CMAKE_DATA):
+    from skbuild.constants import CMAKE_INSTALL_DIR as SKBUILD_CMAKE_INSTALL_DIR
     _cmake_data = os.path.abspath(os.path.join(
-        os.path.dirname(__file__), '../_skbuild/cmake-install/cmake/data'))
+        os.path.dirname(__file__), '..', SKBUILD_CMAKE_INSTALL_DIR, 'cmake/data'))
     if os.path.exists(_cmake_data):
         CMAKE_DATA = _cmake_data
 
