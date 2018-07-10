e96c0658965afc18eb5b4489ad2126aa761e86fc
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-07-10 05:11:39 -0400

    style: refactor test_distribution introducing _check_cmake_install function

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -8,15 +8,7 @@ from path import Path
 DIST_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../dist'))
 
 
-@pytest.mark.skipif(not Path(DIST_DIR).exists(), reason="dist directory does not exist")
-def test_wheel(virtualenv, tmpdir):
-    wheels = Path(DIST_DIR).files(pattern="*.whl")
-    if not wheels:
-        pytest.skip("no wheel available")
-    assert len(wheels) == 1
-
-    virtualenv.run("pip install %s" % wheels[0])
-
+def _check_cmake_install(virtualenv, tmpdir):
     expected_version = "3.11.2"
 
     for executable_name in ["cmake", "cpack", "ctest"]:
@@ -26,9 +18,21 @@ def test_wheel(virtualenv, tmpdir):
 
     test_script = tmpdir.join("test_cmake.cmake")
     test_script.write(textwrap.dedent(r"""
-    message("${CMAKE_COMMAND}")
-    """))
+        message("${CMAKE_COMMAND}")
+        """))
 
     output = virtualenv.run("cmake -P %s" % str(test_script), capture=True)
     expected = os.path.realpath(virtualenv.virtualenv).replace(os.sep, "/")
     assert output[:len(expected)].lower() == expected.lower()
+
+
+@pytest.mark.skipif(not Path(DIST_DIR).exists(), reason="dist directory does not exist")
+def test_wheel(virtualenv, tmpdir):
+    wheels = Path(DIST_DIR).files(pattern="*.whl")
+    if not wheels:
+        pytest.skip("no wheel available")
+    assert len(wheels) == 1
+
+    virtualenv.run("pip install %s" % wheels[0])
+
+    _check_cmake_install(virtualenv, tmpdir)
