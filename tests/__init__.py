15f648da89b94e5cb20192723b6f7841ad3fa048
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-07-23 19:36:16 -0400

    test_cmake_https: skip for macOS CMake binaries <= 3.7.1 having broken SSL support

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,9 @@
 
+import os
 import sys
 
 from contextlib import contextmanager
+from skbuild.constants import CMAKE_BUILD_DIR
 
 
 @contextmanager
@@ -10,3 +12,9 @@ def push_argv(argv):
     sys.argv = argv
     yield
     sys.argv = old_argv
+
+
+def build_from_source():
+    """Return True if CMake was built from source."""
+    marker = os.path.join(os.path.dirname(__file__), "..", CMAKE_BUILD_DIR, "CMakeProject-build")
+    return os.path.exists(marker)
