cea309471b151e23d3af487b2194528e7bfa2934
AliAskari <mohammadali.askarih@gmail.com>
2019-05-11 06:30:40 -0400

    update to CMake 3.14.3

--- CMakeUrls.cmake
+++ CMakeUrls.cmake
@@ -1,11 +1,11 @@
 
 #-----------------------------------------------------------------------------
 # CMake sources
-set(unix_source_url       "https://github.com/Kitware/CMake/releases/download/v3.13.3/cmake-3.13.3.tar.gz")
-set(unix_source_sha256    "665f905036b1f731a2a16f83fb298b1fb9d0f98c382625d023097151ad016b25")
+set(unix_source_url       "https://github.com/Kitware/CMake/releases/download/v3.14.3/cmake-3.14.3.tar.gz")
+set(unix_source_sha256    "215d0b64e81307182b29b63e562edf30b3875b834efdad09b3fcb5a7d2f4b632")
 
-set(windows_source_url    "https://github.com/Kitware/CMake/releases/download/v3.13.3/cmake-3.13.3.zip")
-set(windows_source_sha256 "4de19b7f74e7de444df710423bb04dcf3da01bc31ba9b867fc04c77d88ce44b6")
+set(windows_source_url    "https://github.com/Kitware/CMake/releases/download/v3.14.3/cmake-3.14.3.zip")
+set(windows_source_sha256 "06c0ea5d486d5b837daa50c0bf76f2d375e1053790942ea78c42c5f8bb56aa62")
 
 #-----------------------------------------------------------------------------
 # CMake binaries
@@ -13,14 +13,14 @@ set(windows_source_sha256 "4de19b7f74e7de444df710423bb04dcf3da01bc31ba9b867fc04c
 set(linux32_binary_url    "NA")  # Linux 32-bit binaries not available
 set(linux32_binary_sha256 "NA")
 
-set(linux64_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.13.3/cmake-3.13.3-Linux-x86_64.tar.gz")
-set(linux64_binary_sha256 "78227de38d574d4d19093399fd4b40a4fb0a76cbfc4249783a969652ce515270")
+set(linux64_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.14.3/cmake-3.14.3-Linux-x86_64.tar.gz")
+set(linux64_binary_sha256 "29faa62fb3a0b6323caa3d9557e1a5f1205614c0d4c5c2a9917f16a74f7eff68")
 
-set(macosx_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.13.3/cmake-3.13.3-Darwin-x86_64.tar.gz")
-set(macosx_binary_sha256 "cee923ec7a88350f78dac06f0dfae5dec3c9cef331686acfd36ef7dd5aea84db")
+set(macosx_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.14.3/cmake-3.14.3-Darwin-x86_64.tar.gz")
+set(macosx_binary_sha256 "3672bdf0aa492c6f1061f3c2d6ae670c3f6ac1eca56a3d6ed5dbe749e2b5a9c9")
 
-set(win32_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.13.3/cmake-3.13.3-win32-x86.zip")
-set(win32_binary_sha256 "1382a32494b49d0554268a6d4c321865ace7e66c9189323c4f43630f067cf135")
+set(win32_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.14.3/cmake-3.14.3-win32-x86.zip")
+set(win32_binary_sha256 "1a2d8ffdb63d3b53fe3a93e378ce2e4a36656bdce015cc3e561920e406ebf497")
 
-set(win64_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.13.3/cmake-3.13.3-win64-x64.zip")
-set(win64_binary_sha256 "6edc6686cd29572984497bb14de338b496604b5029c75321bf559d47664b38ab")
+set(win64_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.14.3/cmake-3.14.3-win64-x64.zip")
+set(win64_binary_sha256 "53856665d3302aca1684e20eaf423062ab192665a9714a27b10f2d6cd01f3454")
