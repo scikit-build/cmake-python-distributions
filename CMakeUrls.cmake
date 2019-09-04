ce93c9e41dd7a0334b87189a2532dce52896e308
Henry Schreiner <hschrein@cern.ch>
2019-09-04 13:39:19 -0400

    Update to CMake 3.15.3

--- CMakeUrls.cmake
+++ CMakeUrls.cmake
@@ -1,11 +1,11 @@
 
 #-----------------------------------------------------------------------------
 # CMake sources
-set(unix_source_url       "https://github.com/Kitware/CMake/releases/download/v3.15.2/cmake-3.15.2.tar.gz")
-set(unix_source_sha256    "539088cb29a68e6d6a8fba5c00951e5e5b1a92c68fa38a83e1ed2f355933f768")
+set(unix_source_url       "https://github.com/Kitware/CMake/releases/download/v3.15.3/cmake-3.15.3.tar.gz")
+set(unix_source_sha256    "13958243a01365b05652fa01b21d40fa834f70a9e30efa69c02604e64f58b8f5")
 
-set(windows_source_url    "https://github.com/Kitware/CMake/releases/download/v3.15.2/cmake-3.15.2.zip")
-set(windows_source_sha256 "3aac16f6e8049d8b93069474e954405c91a8bfdd88da383c296a03e287cbbf0d")
+set(windows_source_url    "https://github.com/Kitware/CMake/releases/download/v3.15.3/cmake-3.15.3.zip")
+set(windows_source_sha256 "0c70e4b50aba829d9283ad77af1ca58d976fcd2811cdb99687be55be427daad9")
 
 #-----------------------------------------------------------------------------
 # CMake binaries
@@ -13,14 +13,14 @@ set(windows_source_sha256 "3aac16f6e8049d8b93069474e954405c91a8bfdd88da383c296a0
 set(linux32_binary_url    "NA")  # Linux 32-bit binaries not available
 set(linux32_binary_sha256 "NA")
 
-set(linux64_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.15.2/cmake-3.15.2-Linux-x86_64.tar.gz")
-set(linux64_binary_sha256 "f8cbec2abc433938bd9378b129d1d288bb33b8b5a277afe19644683af6e32a59")
+set(linux64_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.15.3/cmake-3.15.3-Linux-x86_64.tar.gz")
+set(linux64_binary_sha256 "020812a9f87293482cec51fdf44f41cc47e794de568f945a8175549d997e1760")
 
-set(macosx_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.15.2/cmake-3.15.2-Darwin-x86_64.tar.gz")
-set(macosx_binary_sha256 "7ec056d641b8cbea98b220efdcc99da1991758a370063dcac3a0cd388d6b30b6")
+set(macosx_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.15.3/cmake-3.15.3-Darwin-x86_64.tar.gz")
+set(macosx_binary_sha256 "f5edcf630ef6b1fb6c81ea971e043318b5d4776678701e479841fb58a9c25236")
 
-set(win32_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.15.2/cmake-3.15.2-win32-x86.zip")
-set(win32_binary_sha256 "afc698e6470358abdeff56231acd582762a6abbdf8c27efc0938b5f7cc842cfa")
+set(win32_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.15.3/cmake-3.15.3-win32-x86.zip")
+set(win32_binary_sha256 "711828fa6744041ea399bbe32e18472a1894594f8b08ce1d96a9cc2d20fcbc18")
 
-set(win64_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.15.2/cmake-3.15.2-win64-x64.zip")
-set(win64_binary_sha256 "28332003b55ed76a8928e9d071d9cf89cba040bea7c4e4c2100204ecbed7b2cf")
+set(win64_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.15.3/cmake-3.15.3-win64-x64.zip")
+set(win64_binary_sha256 "a18d96b7839ac3294e5e9f464f0af4c8336a16cd5f95e69a90a259207d7e5177")
