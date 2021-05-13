22dc0c5f42989f0b94f4cf76325f9b390f121b1c
Matthieu Darbois <mayeut@users.noreply.github.com>
2021-05-14 01:37:22 +0200

    fix: building from sdist fails (#136)
    * Add test_source_distribution
    
    * Fix versioneer import for sdist build in an isolated pip build
    
    * Fix version returned by versioneer when "post" is already in an existing tag

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -3,7 +3,7 @@ import os
 import pytest
 import textwrap
 
-from path import Path
+from path import Path, matchers
 
 DIST_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../dist'))
 
@@ -24,6 +24,21 @@ def _check_cmake_install(virtualenv, tmpdir):
     output = virtualenv.run("cmake -P %s" % str(test_script), capture=True)
     expected = os.path.realpath(virtualenv.virtualenv).replace(os.sep, "/")
     assert output[:len(expected)].lower() == expected.lower()
+
+
+@pytest.mark.skipif(not Path(DIST_DIR).exists(), reason="dist directory does not exist")
+def test_source_distribution(virtualenv, tmpdir):
+    sdists = Path(DIST_DIR).files(match=matchers.CaseInsensitive("*.tar.gz"))
+    if not sdists:
+        pytest.skip("no source distribution available")
+    assert len(sdists) == 1
+
+    if "SETUP_CMAKE_ARGS" in os.environ:
+        virtualenv.env["SKBUILD_CONFIGURE_OPTIONS"] = os.environ["SETUP_CMAKE_ARGS"]
+    virtualenv.run("pip install %s" % sdists[0])
+    assert "cmake" in virtualenv.installed_packages()
+
+    _check_cmake_install(virtualenv, tmpdir)
 
 
 @pytest.mark.skipif(not Path(DIST_DIR).exists(), reason="dist directory does not exist")
