4730aeee240917303f293dffc89a8d8d5a4787c4
Matthieu Darbois <mayeut@users.noreply.github.com>
2021-06-03 11:46:18 +0200

    refactor: split requirements (#148)

--- scripts/manylinux2014-build-and-test-wheel.sh
+++ scripts/manylinux2014-build-and-test-wheel.sh
@@ -9,17 +9,6 @@ export PATH="${MANYLINUX_PYTHON_BIN}:$PATH"
 cd /io
 ./scripts/manylinux2014-build-and-install-openssl.sh /tmp/openssl-install shared
 
-MACHINE=$(uname -m)
-if [ "${MACHINE}" == "s390x" ] || [ "${MACHINE}" == "ppc64le" ]; then
-    # build cryptography from sources
-    yum install -y libffi-devel
-    curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable --profile minimal --no-modify-path
-    export PATH=${HOME}/.cargo/bin:${PATH}
-    ./scripts/manylinux2014-build-and-install-openssl.sh /tmp/openssl-cryptography no-shared -fPIC
-    CFLAGS="-I/tmp/openssl-cryptography/include" LDFLAGS="-L/tmp/openssl-cryptography/lib" ${MANYLINUX_PYTHON_BIN}/pip wheel --no-binary :all: cryptography
-    ${MANYLINUX_PYTHON_BIN}/pip install ./cryptography*.whl
-fi
-
 ci_before_install() {
     ${MANYLINUX_PYTHON_BIN}/python scripts/ssl-check.py
     ${MANYLINUX_PYTHON_BIN}/pip install scikit-ci scikit-ci-addons scikit-build
