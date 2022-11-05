4f08af2018206628db924e807b83330bc7851afb
mayeut <mayeut@users.noreply.github.com>
2022-11-05 08:24:42 +0100

    chore(build): move to scikitbuild-core

--- src/cmake/__init__.py
+++ src/cmake/__init__.py
@@ -11,24 +11,7 @@ del get_versions
 CMAKE_DATA = os.path.join(os.path.dirname(__file__), 'data')
 
 
-# Support running tests from the source tree
-if not os.path.exists(CMAKE_DATA):
-    from skbuild.constants import CMAKE_INSTALL_DIR as SKBUILD_CMAKE_INSTALL_DIR
-    from skbuild.constants import set_skbuild_plat_name
-
-    if sys.platform.startswith("darwin"):
-        # Since building the project specifying --plat-name or CMAKE_OSX_* variables
-        # leads to different SKBUILD_DIR, the code below attempt to guess the most
-        # likely plat-name.
-        _skbuild_dirs = os.listdir(os.path.join(os.path.dirname(__file__), '..', '..', '_skbuild'))
-        if _skbuild_dirs:
-            _likely_plat_name = '-'.join(_skbuild_dirs[0].split('-')[:3])
-            set_skbuild_plat_name(_likely_plat_name)
-
-    _cmake_data = os.path.abspath(os.path.join(
-        os.path.dirname(__file__), '..', '..', SKBUILD_CMAKE_INSTALL_DIR(), 'src/cmake/data'))
-    if os.path.exists(_cmake_data):
-        CMAKE_DATA = _cmake_data
+assert os.path.exists(CMAKE_DATA)
 
 CMAKE_BIN_DIR = os.path.join(CMAKE_DATA, 'bin')
 CMAKE_DOC_DIR = os.path.join(CMAKE_DATA, 'doc')
