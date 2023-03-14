7e6cb6c74f06f131860551e84e4cfeeae02e18e3
Matthieu Darbois <mayeut@users.noreply.github.com>
2023-03-14 20:28:35 +0100

    Update to CMake 3.25.3 (#337)

--- CMakeUrls.cmake
+++ CMakeUrls.cmake
@@ -1,11 +1,11 @@
 
 #-----------------------------------------------------------------------------
 # CMake sources
-set(unix_source_url          "https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2.tar.gz")
-set(unix_source_sha256       "c026f22cb931dd532f648f087d587f07a1843c6e66a3dfca4fb0ea21944ed33c")
+set(unix_source_url          "https://github.com/Kitware/CMake/releases/download/v3.25.3/cmake-3.25.3.tar.gz")
+set(unix_source_sha256       "cc995701d590ca6debc4245e9989939099ca52827dd46b5d3592f093afe1901c")
 
-set(windows_source_url       "https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2.zip")
-set(windows_source_sha256    "2781c207c3c64ea8a829b8fc0229374fc874b68fb1264cff99055e5bce79bbae")
+set(windows_source_url       "https://github.com/Kitware/CMake/releases/download/v3.25.3/cmake-3.25.3.zip")
+set(windows_source_sha256    "a1943dd0dfc5fbd7ef10e507051bacc5f76ef6d8f1a188e6e7ab3a57df220eba")
 
 #-----------------------------------------------------------------------------
 # CMake binaries
@@ -13,17 +13,17 @@ set(windows_source_sha256    "2781c207c3c64ea8a829b8fc0229374fc874b68fb1264cff99
 set(linux32_binary_url       "NA")  # Linux 32-bit binaries not available
 set(linux32_binary_sha256    "NA")
 
-set(linux64_binary_url       "https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2-linux-x86_64.tar.gz")
-set(linux64_binary_sha256    "783da74f132fd1fea91b8236d267efa4df5b91c5eec1dea0a87f0cf233748d99")
+set(linux64_binary_url       "https://github.com/Kitware/CMake/releases/download/v3.25.3/cmake-3.25.3-linux-x86_64.tar.gz")
+set(linux64_binary_sha256    "d4d2ba83301b215857d3b6590cd4434a414fa151c5807693abe587bd6c03581e")
 
-set(macos10_10_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2-macos10.10-universal.tar.gz")
-set(macos10_10_binary_sha256 "a988e2a69c1d105987f12782ee0fa80d6be941b3e1a68b4bd6a661f0fdb56d75")
+set(macos10_10_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.25.3/cmake-3.25.3-macos10.10-universal.tar.gz")
+set(macos10_10_binary_sha256 "ea37a83cd329224c38a1938d2cf7b2656ad971dea9cf759238652746213661f8")
 
-set(win32_binary_url         "https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2-windows-i386.zip")
-set(win32_binary_sha256      "833abaa21bb673491def058cc38834fbd606d525ab271f37a3b8a3aed586c4a0")
+set(win32_binary_url         "https://github.com/Kitware/CMake/releases/download/v3.25.3/cmake-3.25.3-windows-i386.zip")
+set(win32_binary_sha256      "2e66cf7cbd35f392f6b9406910c68f4adfca1de9ab7a2fc4cec658c5c5cd9190")
 
-set(win64_binary_url         "https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2-windows-x86_64.zip")
-set(win64_binary_sha256      "0db9d3cebf894f64751141253fb9d9e310f325e2e03044f61249a359d6adf301")
+set(win64_binary_url         "https://github.com/Kitware/CMake/releases/download/v3.25.3/cmake-3.25.3-windows-x86_64.zip")
+set(win64_binary_sha256      "d129425d569140b729210f3383c246dec19c4183f7d0afae1837044942da3b4b")
 
-set(winarm64_binary_url      "https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2-windows-arm64.zip")
-set(winarm64_binary_sha256   "c54fb253ae184b391d5366b958c38b282d5f9b6a5854643c28e6887f5fd92590")
+set(winarm64_binary_url      "https://github.com/Kitware/CMake/releases/download/v3.25.3/cmake-3.25.3-windows-arm64.zip")
+set(winarm64_binary_sha256   "3498fea26257eebfbfc89ed17963f3d8d83c19362b90fb23517842de777a522a")
--- README.rst
+++ README.rst
@@ -11,7 +11,7 @@ The suite of CMake tools were created by Kitware in response to the need
 for a powerful, cross-platform build environment for open-source projects
 such as ITK and VTK.
 
-The CMake python wheels provide `CMake 3.25.2 <https://cmake.org/cmake/help/v3.25/index.html>`_.
+The CMake python wheels provide `CMake 3.25.3 <https://cmake.org/cmake/help/v3.25/index.html>`_.
 
 Latest Release
 --------------
