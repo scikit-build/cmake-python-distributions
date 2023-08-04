326cd9a783ce8390064bc8518d3894cda9cacc90
Matthieu Darbois <mayeut@users.noreply.github.com>
2023-08-04 22:21:26 +0200

    Update to OpenSSL 3.0.10 (#387)
    Created by update_openssl_version.py

--- scripts/manylinux-build-and-install-openssl.sh
+++ scripts/manylinux-build-and-install-openssl.sh
@@ -10,8 +10,8 @@ set -o pipefail
 MY_DIR=$(dirname "${BASH_SOURCE[0]}")
 source $MY_DIR/utils.sh
 
-OPENSSL_ROOT=openssl-3.0.9
-OPENSSL_HASH=eb1ab04781474360f77c318ab89d8c5a03abc38e63d65a603cabbf1b00a1dc90
+OPENSSL_ROOT=openssl-3.0.10
+OPENSSL_HASH=1761d4f5b13a1028b9b6f3d4b8e17feb0cedc9370f6afe61d7193d2cdce83323
 
 cd /tmp
 
