0e0d94dcbc10d9b5d33748ae8e89a497fe9ba707
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2024-02-19 17:46:45 -0500

    fix: Support default "redirect" mode for editable install with Python >= 3.4
    Add support for editable install using Python 3.4 (first version with
    built-in pathlib support)

--- src/cmake/__init__.py
+++ src/cmake/__init__.py
@@ -3,6 +3,12 @@ import os
 import subprocess
 import sys
 
+if sys.version_info >= (3, 4):
+    if sys.version_info < (3, 10):
+        from importlib_metadata import distribution
+    else:
+        from importlib.metadata import distribution
+
 from ._version import version as __version__
 
 __all__ = ["__version__", "CMAKE_DATA", "CMAKE_BIN_DIR", "CMAKE_DOC_DIR", "CMAKE_SHARE_DIR", "cmake", "cpack", "ctest"]
@@ -12,9 +18,22 @@ def __dir__():
     return __all__
 
 
-CMAKE_DATA = os.path.join(os.path.dirname(__file__), 'data')
+if sys.version_info >= (3, 4):
+    cmake_executable_path = None
+    for script in distribution("cmake").files:
+        if str(script).startswith("cmake/data/bin/cmake"):
+            if sys.version_info < (3, 6):
+                # pre-3.6 behavior is strict
+                resolved_script = script.locate().resolve()
+            else:
+                resolved_script = script.locate().resolve(strict=True)
+            cmake_executable_path = resolved_script.parents[1]
+            break
+    CMAKE_DATA = cmake_executable_path if cmake_executable_path else None
+else:
+    CMAKE_DATA = os.path.join(os.path.dirname(__file__), "data")
 
-
+assert CMAKE_DATA is not None
 assert os.path.exists(CMAKE_DATA)
 
 CMAKE_BIN_DIR = os.path.join(CMAKE_DATA, 'bin')
