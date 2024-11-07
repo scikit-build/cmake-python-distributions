620b303dbd30926e360afe099bb0349f43c1e344
Henry Schreiner <HenrySchreinerIII@gmail.com>
2024-11-07 14:58:04 -0500

    docs: move off of abandoned dep (#556)
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ blockdiag_antialias = True
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.blockdiag']
+extensions = ['sphinxcontrib.mermaid']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
