d1aa067537ad76e3756de3248f1ad72f45a18d85
sculpordwarf <69463323+sculpordwarf@users.noreply.github.com>
2025-11-20 13:52:19 +0000

    [v3.x] Update to CMake 3.31.10 (#651)
    * Update to CMake 3.31.9
    
    * chore: update to CMake 3.31.10
    
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>
    
    * ci: bump CI images
    
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>
    
    ---------
    
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>
    Co-authored-by: scikit-build-app-bot[bot] <173546081+scikit-build-app-bot[bot]@users.noreply.github.com>
    Co-authored-by: Henry Schreiner <henryfs@princeton.edu>

--- CMakeUrls.cmake
+++ CMakeUrls.cmake
@@ -1,11 +1,11 @@
 
 #-----------------------------------------------------------------------------
 # CMake sources
-set(unix_source_url          "https://github.com/Kitware/CMake/releases/download/v3.31.6/cmake-3.31.6.tar.gz")
-set(unix_source_sha256       "653427f0f5014750aafff22727fb2aa60c6c732ca91808cfb78ce22ddd9e55f0")
+set(unix_source_url          "https://github.com/Kitware/CMake/releases/download/v3.31.10/cmake-3.31.10.tar.gz")
+set(unix_source_sha256       "cf06fadfd6d41fa8e1ade5099e54976d1d844fd1487ab99942341f91b13d3e29")
 
-set(windows_source_url       "https://github.com/Kitware/CMake/releases/download/v3.31.6/cmake-3.31.6.zip")
-set(windows_source_sha256    "106df3d82f20aaa52a5f575c836effc5713a2fe3235f38e9be5c8a14b8d35530")
+set(windows_source_url       "https://github.com/Kitware/CMake/releases/download/v3.31.10/cmake-3.31.10.zip")
+set(windows_source_sha256    "5e643cba4d96dc2dcd0eb232d064e9963c9a47a0859832e19a1c5ceee3b984f9")
 
 #-----------------------------------------------------------------------------
 # CMake binaries
@@ -13,17 +13,17 @@ set(windows_source_sha256    "106df3d82f20aaa52a5f575c836effc5713a2fe3235f38e9be
 set(linux32_binary_url       "NA")  # Linux 32-bit binaries not available
 set(linux32_binary_sha256    "NA")
 
-set(linux64_binary_url       "https://github.com/Kitware/CMake/releases/download/v3.31.6/cmake-3.31.6-linux-x86_64.tar.gz")
-set(linux64_binary_sha256    "5a1133ff103c71eb5120e2cc3de922733e7d8a26a98ae716397e8676adb367bf")
+set(linux64_binary_url       "https://github.com/Kitware/CMake/releases/download/v3.31.10/cmake-3.31.10-linux-x86_64.tar.gz")
+set(linux64_binary_sha256    "3cb3dd247b6a1de2d0f4b20c6fd4326c9024e894cebc9dc8699758887e566ca7")
 
-set(macos10_10_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.31.6/cmake-3.31.6-macos10.10-universal.tar.gz")
-set(macos10_10_binary_sha256 "dc49fe67e0841e1d21b9e694d4858bbc06118165be5ebd8da0f2028216d56e6c")
+set(macos10_10_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.31.10/cmake-3.31.10-macos10.10-universal.tar.gz")
+set(macos10_10_binary_sha256 "45054456ff4eb3ea844568590426c0c738d343ed081fbf86b0544cbcde3a4dec")
 
-set(win32_binary_url         "https://github.com/Kitware/CMake/releases/download/v3.31.6/cmake-3.31.6-windows-i386.zip")
-set(win32_binary_sha256      "005ef676975aa0f8348fc2c8011a913f9676f07f602ca624ee0811b427d3fd84")
+set(win32_binary_url         "https://github.com/Kitware/CMake/releases/download/v3.31.10/cmake-3.31.10-windows-i386.zip")
+set(win32_binary_sha256      "50fb5cc4b47cc82636b9de51bf10f1d422698f96e6e190017f6f0603a3b0e2fd")
 
-set(win64_binary_url         "https://github.com/Kitware/CMake/releases/download/v3.31.6/cmake-3.31.6-windows-x86_64.zip")
-set(win64_binary_sha256      "d163cd3ab4959b0a53fa8988f2ddbd2e6c501658201e6a154386bad9dbe4f836")
+set(win64_binary_url         "https://github.com/Kitware/CMake/releases/download/v3.31.10/cmake-3.31.10-windows-x86_64.zip")
+set(win64_binary_sha256      "13d1a463d7130df5339baedd63d8ae990aaf385062b2f42f372796143ae94086")
 
-set(winarm64_binary_url      "https://github.com/Kitware/CMake/releases/download/v3.31.6/cmake-3.31.6-windows-arm64.zip")
-set(winarm64_binary_sha256   "fa648fd417f44e6cb08928964a480ade0d18df421f9b623639dba22f9b301e4e")
+set(winarm64_binary_url      "https://github.com/Kitware/CMake/releases/download/v3.31.10/cmake-3.31.10-windows-arm64.zip")
+set(winarm64_binary_sha256   "5bf429626c0fbcfc38a4f7247736e0fedd625197681b07d6b9f5a702f557eb3e")
--- docs/make_a_release.rst
+++ docs/make_a_release.rst
@@ -44,8 +44,8 @@ Prerequisites
 
   .. code:: console
 
-    $ git tag --sign -m 'cmake-python-distributions 3.31.6' 3.31.6 main
-    $ git push origin 3.31.6
+    $ git tag --sign -m 'cmake-python-distributions 3.31.10' 3.31.10 main
+    $ git push origin 3.31.10
 
   .. warning::
 
